b4ec9c8afcdc4734c33f0ed9c4fe38f9af08d684
tomlai19852004 <tomlaihosum@hotmail.com>
2018-06-06 17:24:12 +0800

    added check data

---
+++ check_data.py
@@ -0,0 +1,7 @@
+from basic.read_data import DataSet, read_data, get_squad_data_filter
+from basic.main import _get_args, Config
+
+
+if __name__ == "__main__":
+	args = _get_args()
+	print(type(args))
