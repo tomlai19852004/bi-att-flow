c88bf6dac1e657d348b66b16c3a3b43cfbffab62
tomlai19852004 <tomlaihosum@hotmail.com>
2018-06-17 21:10:56 +0800

    update port

--- run-demo.py
+++ run-demo.py
@@ -49,4 +49,4 @@ def submit():
     return jsonify(result=answer)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1995, threaded=True)
+    app.run(host="0.0.0.0", port=8000, threaded=True)
